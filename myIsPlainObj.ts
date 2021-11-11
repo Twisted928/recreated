15f1fda961eb5f68eed5b58bcd0e385ea0e42058
Twisted <dcmTiwsted@aliyun.com>
2021-11-11 16:02:34 +0800

    undate isPlainObj(lodash version & redux version)

--- myIsPlainObj.ts
+++ myIsPlainObj.ts
@@ -1,17 +1,37 @@
 /**
  * plain object 无格式对象(普通对象),通过对象字面量或者new Object()创建的对象
- * @author redux
+ * 即：判断 obj 的构造函数是不是 Object
+ *
  */
 
 function myIsPlainObj(obj: any): boolean {
-  if (obj === null || obj === undefined || typeof obj !== "object") {
+  if (
+    !obj ||
+    typeof obj !== "object" ||
+    {}.toString.call(obj) !== "[object Object]" // 排除Array,Date等引用类型
+  ) {
     return false;
   }
 
-  let proto = obj;
-  while (Object.getPrototypeOf(proto) !== null) {
-    proto = Object.getPrototypeOf(proto);
+  // Lodash version
+  const proto = Object.getPrototypeOf(obj);
+  if (proto === null) {
+    return true;
   }
-  // 判断obj的prototype是否为Object
-  return Object.getPrototypeOf(obj) === proto;
+
+  const ctor: any = {}.hasOwnProperty.call("constructor") & proto.constructor;
+  return (
+    typeof ctor === "function" &&
+    ctor instanceof ctor &&
+    ctor.toString() === Object.toString()
+  );
+
+  /**
+   *  @author Redux
+   *  let proto = Object.getPrototypeOf(obj);
+   *  while (Object.getPrototypeOf(proto) !== null) {
+   *    proto = Object.getPrototypeOf(obj);
+   *  }
+   *  return Object.getPrototypeOf(Obj) === Object.getPrototypeOf(proto);
+   */
 }
